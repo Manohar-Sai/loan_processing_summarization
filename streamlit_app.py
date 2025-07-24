7a504a348515c7fcf5a72e708ecb18516769bb61
Manohar-Sai <56510069+Manohar-Sai@users.noreply.github.com>
2025-07-25 00:17:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -98,7 +98,7 @@ if submitted:
                 </style>
             </head>
             <body>
-                {md_report.replace("\n", "<br>")}
+                {md_report.replace(r"\n", "<br>")}
             </body>
             </html>
             """
